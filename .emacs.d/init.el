eb5c3986714e2ca2fe556540318b3576870ac600
Shuichiro Kamiya <notsunohito@gmail.com>
2015-02-20 12:59:48 +0900

    Add swap-screen-with-cursor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -253,3 +253,21 @@
 )
 
 (setq line-spacing 0.3)
+
+(defun swap-screen()
+  "Swap two screen,leaving cursor at current window."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (nextbuf (window-buffer (next-window))))
+    (set-window-buffer (next-window) (window-buffer))
+    (set-window-buffer thiswin nextbuf)))
+(defun swap-screen-with-cursor()
+  "Swap two screen,with cursor in same buffer."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (thisbuf (window-buffer)))
+    (other-window 1)
+    (set-window-buffer thiswin (window-buffer))
+    (set-window-buffer (selected-window) thisbuf)))
+;; (global-set-key (kbd "C-c C-T") 'swap-screen)
+(global-set-key (kbd "C-c C-T") 'swap-screen-with-cursor)
