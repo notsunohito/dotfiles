dd80e5585103e1c2dc77e961574efe6f276f5e5a
Shuichiro Kamiya <notsunohito@gmail.com>
2015-03-12 23:04:10 +0900

    Add typescript-mode(tss)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -271,3 +271,20 @@
     (set-window-buffer (selected-window) thisbuf)))
 ;; (global-set-key (kbd "C-c C-T") 'swap-screen)
 (global-set-key (kbd "C-c C-T") 'swap-screen-with-cursor)
+
+;; If use bundled typescript.el,
+(require 'typescript)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+
+(require 'tss)
+
+;; Key binding
+(setq tss-popup-help-key "C-:")
+(setq tss-jump-to-definition-key "C->")
+(setq tss-implement-definition-key "C-c i")
+
+;; Make config suit for you. About the config item, eval the following sexp.
+;; (customize-group "tss")
+
+;; Do setting recommemded configuration
+(tss-config-default)
