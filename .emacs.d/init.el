89eff41bd5e24f0bbb1b57b058a9679fac4e431f
Shuichiro Kamiya <notsunohito@gmail.com>
2015-03-19 23:08:31 +0900

    reopen-with-sudo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -288,3 +288,11 @@
 
 ;; Do setting recommemded configuration
 (tss-config-default)
+
+(defun reopen-with-sudo ()
+  "Reopen current buffer-file with sudo using tramp."
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (if file-name
+        (find-alternate-file (concat "/sudo::" file-name))
+      (error "Cannot get a file name"))))
